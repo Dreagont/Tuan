9211dff1b8f312ba3ad7ab2c70083bc01c3eea0d
patuaans <phananhtuan849@gmail.com>
2023-12-15 23:02:20 +0700

    Delete topic

--- app/src/main/java/com/example/quizletfinal/TopicActivity.kt
+++ app/src/main/java/com/example/quizletfinal/TopicActivity.kt
@@ -8,8 +8,8 @@ import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.quizletfinal.adapters.CardAdapter
@@ -17,18 +17,23 @@ import com.example.quizletfinal.models.Card
 import com.example.quizletfinal.models.Folder
 import com.example.quizletfinal.models.OnItemClickListener
 import com.example.quizletfinal.models.Topic
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 import java.util.Locale
 
-class TopicActivity : AppCompatActivity(), OnItemClickListener, TextToSpeech.OnInitListener { //here
-    private lateinit var textToSpeech: TextToSpeech //here
+class TopicActivity : AppCompatActivity(), OnItemClickListener, TextToSpeech.OnInitListener {
+    private lateinit var textToSpeech: TextToSpeech
+    private lateinit var auth: FirebaseAuth
+    private var receivedTopic: Topic? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_topic)
 
-        val receivedTopic = intent.getParcelableExtra<Topic>("topicData")
+        receivedTopic = intent.getParcelableExtra("topicData")
 
         if (receivedTopic != null) {
+            val deleteButton = findViewById<TextView>(R.id.btnDeleteTopic)
             val closeButton = findViewById<Button>(R.id.btnClose)
             val topicName = findViewById<TextView>(R.id.txtTopicName)
             val userName = findViewById<TextView>(R.id.txtUsername)
@@ -39,10 +44,12 @@ class TopicActivity : AppCompatActivity(), OnItemClickListener, TextToSpeech.OnI
             val btnTextGame = findViewById<LinearLayout>(R.id.btnTextGame)
             val cardListView = findViewById<RecyclerView>(R.id.cardListView)
 
-            val username = receivedTopic.username
-            val topicNameText = receivedTopic.title
-            val topicDescription = receivedTopic.description
-            val termNumberValue = receivedTopic.cards.size
+            auth = FirebaseAuth.getInstance()
+
+            val username = receivedTopic!!.username
+            val topicNameText = receivedTopic!!.title
+            val topicDescription = receivedTopic!!.description
+            val termNumberValue = receivedTopic!!.cards.size
             textToSpeech = TextToSpeech(this, this)
 
             topicName.text = topicNameText
@@ -51,7 +58,7 @@ class TopicActivity : AppCompatActivity(), OnItemClickListener, TextToSpeech.OnI
             topicDescriptionView.text = topicDescription
 
             closeButton.setOnClickListener { finish() }
-            val cardList = receivedTopic.cards.values.toList()
+            val cardList = receivedTopic!!.cards.values.toList()
 
             cardListView.layoutManager = LinearLayoutManager(applicationContext)
 
@@ -67,7 +74,7 @@ class TopicActivity : AppCompatActivity(), OnItemClickListener, TextToSpeech.OnI
             multiChoice.setOnClickListener {
                 val intent = Intent(this, TestSettingActivity::class.java)
 
-                intent.putExtra("topicName", receivedTopic.title)
+                intent.putExtra("topicName", receivedTopic!!.title)
                 intent.putExtra("cardList", ArrayList(cardList))
                 intent.putExtra("game", "multi")
 
@@ -77,17 +84,65 @@ class TopicActivity : AppCompatActivity(), OnItemClickListener, TextToSpeech.OnI
             btnTextGame.setOnClickListener {
                 val intent = Intent(this, TestSettingActivity::class.java)
 
-                intent.putExtra("topicName", receivedTopic.title)
+                intent.putExtra("topicName", receivedTopic!!.title)
                 intent.putExtra("cardList", ArrayList(cardList))
                 intent.putExtra("game", "text")
 
                 startActivity(intent)
             }
 
+            deleteButton.setOnClickListener {
+                val topicId = receivedTopic!!.id
+                if (topicId != null) {
+                    showConfirmationDialog(topicId)
+                }
+            }
 
         } else {
             Toast.makeText(this, "No Topic data received", Toast.LENGTH_SHORT).show()
         }
+    }
+    private fun showConfirmationDialog(topicId: String) {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Confirm Deletion")
+        builder.setMessage("Are you sure you want to delete this topic?")
+
+        builder.setPositiveButton("Delete") { dialog, _ ->
+            deleteTopicFromFirebase(topicId)
+            dialog.dismiss()
+        }
+
+        builder.setNegativeButton("Cancel") { dialog, _ ->
+            dialog.dismiss()
+        }
+
+        val dialog = builder.create()
+        dialog.show()
+    }
+
+    private fun deleteTopicFromFirebase(topicId: String) {
+        val currentUser = auth.currentUser
+        val username = receivedTopic?.username
+
+        if (currentUser != null && username != null) {
+            val userReference = FirebaseDatabase.getInstance().getReference("users")
+            val topicReference = userReference.child(username).child("topics").child(topicId)
+
+            topicReference.removeValue()
+                .addOnSuccessListener {
+                    showMessage("Topic deleted successfully")
+                    finish()
+                }
+                .addOnFailureListener { e ->
+                    showMessage("Failed to delete topic: ${e.message}")
+                }
+        } else {
+            showMessage("User not authenticated or topic data not found")
+        }
+    }
+
+    private fun showMessage(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 
     override fun onItemClickListener(topic: Topic) {
@@ -102,10 +157,10 @@ class TopicActivity : AppCompatActivity(), OnItemClickListener, TextToSpeech.OnI
         speakText(card.english)
     }
 
-    private fun speakText(english: String) { //here
+    private fun speakText(english: String) {
         textToSpeech.speak(english, TextToSpeech.QUEUE_FLUSH, null, null)
     }
-    override fun onDestroy() {//here
+    override fun onDestroy() {
         if (textToSpeech.isSpeaking) {
             textToSpeech.stop()
         }
@@ -113,7 +168,7 @@ class TopicActivity : AppCompatActivity(), OnItemClickListener, TextToSpeech.OnI
 
         super.onDestroy()
     }
-    override fun onInit(status: Int) {//here
+    override fun onInit(status: Int) {
         if (status == TextToSpeech.SUCCESS) {
             val result = textToSpeech.setLanguage(Locale.US)
 
@@ -125,4 +180,3 @@ class TopicActivity : AppCompatActivity(), OnItemClickListener, TextToSpeech.OnI
         }
     }
 }
-
