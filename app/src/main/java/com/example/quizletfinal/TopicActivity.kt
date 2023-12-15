fff3e9bcaf64fb8ffbc0bd5eef1004d03be0ee62
Dreagont <huynhannguyen333@gmail.com>
2023-12-15 20:00:58 +0700

    clickable

--- app/src/main/java/com/example/quizletfinal/TopicActivity.kt
+++ app/src/main/java/com/example/quizletfinal/TopicActivity.kt
@@ -2,14 +2,26 @@ package com.example.quizletfinal
 
 import android.content.Intent
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
+import android.util.Log
 import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.quizletfinal.adapters.CardAdapter
+import com.example.quizletfinal.models.Card
+import com.example.quizletfinal.models.Folder
+import com.example.quizletfinal.models.OnItemClickListener
 import com.example.quizletfinal.models.Topic
+import java.util.Locale
 
-class TopicActivity : AppCompatActivity() {
+class TopicActivity : AppCompatActivity(), OnItemClickListener, TextToSpeech.OnInitListener { //here
+    private lateinit var textToSpeech: TextToSpeech //here
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_topic)
@@ -23,11 +35,15 @@ class TopicActivity : AppCompatActivity() {
             val termNumber = findViewById<TextView>(R.id.txtTermNumber)
             val topicDescriptionView = findViewById<TextView>(R.id.txtTopicDescription)
             val flashCardGame = findViewById<LinearLayout>(R.id.btnFlashcard)
+            val  multiChoice= findViewById<LinearLayout>(R.id.btnMultiple)
+
+            val cardListView = findViewById<RecyclerView>(R.id.cardListView)
 
             val username = receivedTopic.username
             val topicNameText = receivedTopic.title
             val topicDescription = receivedTopic.description
             val termNumberValue = receivedTopic.cards.size
+            textToSpeech = TextToSpeech(this, this)
 
             topicName.text = topicNameText
             userName.text = username
@@ -35,13 +51,67 @@ class TopicActivity : AppCompatActivity() {
             topicDescriptionView.text = topicDescription
 
             closeButton.setOnClickListener { finish() }
+            val cardList = receivedTopic.cards.values.toList()
+
+            cardListView.layoutManager = LinearLayoutManager(applicationContext)
+
+            var adapter = CardAdapter(applicationContext, cardList, this)
+            cardListView.adapter = adapter
 
             flashCardGame.setOnClickListener {
-                startActivity(Intent(this, FlashcardActivity::class.java))
+                val intent = Intent(this, FlashcardActivity::class.java)
+                intent.putExtra("topicData", receivedTopic)
+                startActivity(intent)
             }
+
+            multiChoice.setOnClickListener {
+                val intent = Intent(this, TestSettingActivity::class.java)
+
+                intent.putExtra("topicName", receivedTopic.title)
+                intent.putExtra("cardList", ArrayList(cardList))
+                intent.putExtra("game", "multi")
+
+                startActivity(intent)
+            }
+
 
         } else {
             Toast.makeText(this, "No Topic data received", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onItemClickListener(topic: Topic) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onItemClickListener(folder: Folder) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onItemClickListener(card: Card) {
+        speakText(card.english)
+    }
+
+    private fun speakText(english: String) { //here
+        textToSpeech.speak(english, TextToSpeech.QUEUE_FLUSH, null, null)
+    }
+    override fun onDestroy() {//here
+        if (textToSpeech.isSpeaking) {
+            textToSpeech.stop()
+        }
+        textToSpeech.shutdown()
+
+        super.onDestroy()
+    }
+    override fun onInit(status: Int) {//here
+        if (status == TextToSpeech.SUCCESS) {
+            val result = textToSpeech.setLanguage(Locale.US)
+
+            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.e("TextToSpeech", "Language is not supported")
+            }
+        } else {
+            Log.e("TextToSpeech", "Initialization failed")
         }
     }
 }
